6ed6bd2555e384edd7122330e4afa6e78812f05b
Josef Hardi <josef.hardi@gmail.com>
2024-12-09 17:56:48 -0800

    Add default prefixes

--- InitPrefixSheet.js
+++ InitPrefixSheet.js
@@ -9,8 +9,20 @@ function initPrefixSheet(sheetName) {
   prefixSheet.deleteColumns(initialColumnSize+1, 26-initialColumnSize);
   prefixSheet.deleteRows(initialRowSize+1, 1000-initialRowSize);
 
-  const PrefixSheetRange = prefixSheet.getRange(1, 1, initialRowSize, initialColumnSize);
-  PrefixSheetRange.setHorizontalAlignment("left").setVerticalAlignment("top");
+  const prefixSheetRange = prefixSheet.getRange(1, 1, initialRowSize, initialColumnSize);
+  prefixSheetRange.setHorizontalAlignment("left").setVerticalAlignment("top");
+
+  // Set default setting variables
+  setValue(prefixSheet, 1, 1, "rdf");
+  setValue(prefixSheet, 1, 2, "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+  setValue(prefixSheet, 2, 1, "rdfs");
+  setValue(prefixSheet, 2, 2, "http://www.w3.org/2000/01/rdf-schema#");
+  setValue(prefixSheet, 3, 1, "owl");
+  setValue(prefixSheet, 3, 2, "http://www.w3.org/2002/07/owl#");
+  setValue(prefixSheet, 4, 1, "xsd");
+  setValue(prefixSheet, 4, 2, "http://www.w3.org/2001/XMLSchema#");
+  setValue(prefixSheet, 5, 1, "obo");
+  setValue(prefixSheet, 5, 2, "http://purl.obolibrary.org/obo/");
 
   // The sheet is protected only to the owner by default
   const protection = prefixSheet.protect().setDescription('Protected ' + sheetName + ' sheet');
