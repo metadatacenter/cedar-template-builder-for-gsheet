3c30c79375d3262da495724e77f278e50984d6d8
Josef Hardi <josef.hardi@gmail.com>
2024-12-09 17:47:20 -0800

    Set parameter constants

--- InitSettingSheet.js
+++ InitSettingSheet.js
@@ -17,9 +17,9 @@ function initSettingSheet(sheetName) {
   settingSheetRange.setHorizontalAlignment("left").setVerticalAlignment("top");
 
   // Set default setting variables
-  setValue(settingSheet, 1, 1, "ONTOLOGY_IRI");
-  setValue(settingSheet, 2, 1, "ONTOLOGY_ACRONYM");
-  setValue(settingSheet, 3, 1, "BIOPORTAL_API_KEY");
+  setValue(settingSheet, 1, 1, ONTOLOGY_IRI);
+  setValue(settingSheet, 2, 1, ONTOLOGY_ACRONYM);
+  setValue(settingSheet, 3, 1, BIOPORTAL_API_KEY);
 
   // The sheet is protected only to the owner by default
   const protection = settingSheet.protect().setDescription('Protected ' + sheetName + ' sheet');
